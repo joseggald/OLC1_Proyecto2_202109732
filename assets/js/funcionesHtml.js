cc992dd4b00b9aa472aabf782eea2441335fcf59
Jose Eduardo Galdamez Gonzalez <68478652+joseggald@users.noreply.github.com>
2023-04-17 22:21:21 -0600

    Delete assets directory

--- assets/js/funcionesHtml.js
+++
@@ -1,8 +0,0 @@
-function addLineNumbers() {
-    var lines = document.getElementById("input").value.split("\n");
-    var lineNumbers = "";
-    for (var i = 0; i < lines.length; i++) {
-      lineNumbers += i + 1 + "\n";
-    }
-    document.getElementById("line-numbers").innerHTML = lineNumbers;
-  }
